3b421bbbfce2e649cd14140bc4b3730d0106f0aa
ycy <cksdydsla93@gmail.com>
2026-02-03 22:15:31 +0900

    first commit

---
+++ src/main/java/com/yangchanyong/project_v/ProjectVApplication.java
@@ -0,0 +1,13 @@
+package com.yangchanyong.project_v;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectVApplication.class, args);
+	}
+
+}
